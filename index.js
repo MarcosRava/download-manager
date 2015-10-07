335554b9e00c5eaa675ee0fb86cb8ddc7c1f7be5
Marcos Rava <marcosrava@gmail.com>
2015-10-06 23:00:39 -0300

    remove test files

--- index.js
+++ index.js
@@ -2,20 +2,4 @@
 "use strict";
 
 var download = require('./src/download-manager');
-var urls = ['http://download.gimp.org/pub/gimp/v2.8/windows/gimp-2.8.14-setup-1.exe',
-           'http://www.win-rar.com/fileadmin/winrar-versions/winrar/wrar521.exe'];
-
-for (var i in urls) {
-  var url = urls[i];
-  download({url: url, directory: __dirname})
-  .then(function(data) {
-    console.log('code: ', data)
-  })
-  .progress(function(data) {
-    //console.log('progress: ' + data.percent + ' ' + data.file + ' ' + data.speed)
-  })
-  .fail(function(data) {
-    console.log('code error: ', data)
-  })
-}
 module.exports = download;
